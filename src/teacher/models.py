8cd42b20e48ebe7e4878e6999f1f17024de2cc8b
IIioneR <IIioneR@ukr.net>
2020-04-30 00:41:36 +0300

    done homework, created manage command

---
+++ src/teacher/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from faker import Faker
+
+
+class Teacher(models.Model):
+    first_name = models.CharField(max_length=40, null=False)
+    last_name = models.CharField(max_length=20, null=False)
+    email = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f'{self.first_name}, {self.last_name}, {self.email}'
+
+    @classmethod
+    def generate_teacher(cls):
+        faker = Faker()
+        teacher = cls(
+            first_name=faker.first_name(),
+            last_name=faker.last_name(),
+            email=faker.email(),
+        )
+
+        teacher.save()
